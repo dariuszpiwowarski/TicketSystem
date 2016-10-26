c258129384ce692f66a60d9511d68996a1b6fd14
Darek Piwowarski <piwowarskidariusz@gmail.com>
2016-10-26 14:07:13 +0200

    Add validations to the Ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,8 @@
 class Ticket < ApplicationRecord
   belongs_to :category
+  validates :description, :category_id, :email, presence: true
+  validates :description, length: { minimum: 100 }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+  	message: "Email format is not valid"
+  } 
 end
